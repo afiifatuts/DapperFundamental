58d63a6bde30ce556ad00d6c20883fedd6e8f953
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-07-07 18:26:19 +0700

    [5]Linq query syntax, method syntax and pagination

---
+++ DapperFundamental/LambdaNotes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace DapperFundamental
+{
+    public class LambdaNotes
+    {
+        /*
+     public delegate void SayHello(Action callback);
+
+     static void Main(string[] args)
+     {
+         /*
+        Lambda Expression
+       Didefinisikan dalam bahasa pemrograman adalag anonymous function, yang artinya method/function tanpa nama
+
+         2 Tipe Lambda Expression yang digunakan di C#
+       - Lambda Expression : dimana bodynya sebagai expression
+       - Lambda Statement : dimana memiliki block code sebagai bodynya 
+
+
+
+
+         //Return data menggunakan Func<>
+         Func<int, int> square = (int x) => x * x;
+         Console.WriteLine(square(10));
+
+         //tidak mengembalikan apa2 menggunakan Action
+         SayHello sayHello = (Action  callback) =>
+         {
+             Console.WriteLine("Hello world");
+             Console.WriteLine("Hello world");
+             callback();
+         };
+
+         Action callback = () =>
+         {
+             Console.WriteLine("Ini Callback");
+         };
+
+         sayHello(callback);
+
+         sayHello(()=> {
+             Console.WriteLine("Ini adalah Callback Anonymous Function");
+         });
+
+
+ }
+          */
+    }
+}
--- DapperFundamental/Program.cs
+++ DapperFundamental/Program.cs
@@ -2,50 +2,118 @@
 using Npgsql;
 using Dapper;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace DapperFundamental
 {
     public class Program
     {
-
-        public delegate void SayHello(Action callback);
-
         static void Main(string[] args)
         {
             /*
-           Lambda Expression
-          Didefinisikan dalam bahasa pemrograman adalag anonymous function, yang artinya method/function tanpa nama
+            LINQ -> Language-Intergrated Query
+            LINQ ini mendukung banyak data source :
+            - In Memory Object (List, Array)
+            - ADO.NET (DataSet)
+            - Entity Framework
+            - SQL Server
+            - XML Document
+            - Data Source lain lain
 
-            2 Tipe Lambda Expression yang digunakan di C#
-          - Lambda Expression : dimana bodynya sebagai expression
-          - Lambda Statement : dimana memiliki block code sebagai bodynya 
+
+            Linq Providers
+            - LINQ to Objects
+            - LINQ to Entities
+            - LINQ to XML
+            - LINQ to SQL
+            - LINQ to DataSet
+            - dll
 
 
-           */
+            3 Hal untuk Membuat Query LINQ
+            - Data Source (SQL, In Memory Object, XML)
+            - Query (SELECT....)
+            - Execute of the query
 
-            //Return data menggunakan Func<>
-            Func<int, int> square = (int x) => x * x;
-            Console.WriteLine(square(10));
 
-            //tidak mengembalikan apa2 menggunakan Action
-            SayHello sayHello = (Action  callback) =>
+            Kombinasi Query yang kita perlu ketahui:
+            - Query Syntax
+            - Method Syntax
+            - Mixed Syntax
+
+
+            Query Syntax
+            from object in datasource // inisialisasi
+            where condition // kondisi
+            select object  // seleksi
+             */
+
+            //numbers -> datasource(in memory object)
+            var numbers = new List<int> { 2, 3, 4, 5, 6, 7 };
+
+            var querySyntax =
+                from number in numbers // inisialisasi
+                where number > 5 //kondisi
+                select number; //seleksi
+
+
+
+            //foreach( var i in querySyntax)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            /*
+             Method Syntax
+            Datasource.ConditionMethod().SelectionMethod()
+            initialisasi -- kondisi -- seleksi
+
+            var evenNumber = numbers.Where(number => number % 2 == 0).OrderByDescending(i=>i);
+
+            foreach (var n in evenNumber)
             {
-                Console.WriteLine("Hello world");
-                Console.WriteLine("Hello world");
-                callback();
-            };
+                Console.WriteLine(n);
+            }
 
-            Action callback = () =>
+             
+             */
+
+            List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, ProductName = "Hodie", ProductPrice = 100000, Stock = 10 },
+            new Product { Id = 2, ProductName = "Kemeja", ProductPrice = 200000, Stock = 10 },
+            new Product { Id = 3, ProductName = "Celana", ProductPrice = 100000, Stock = 10 },
+            new Product { Id = 4, ProductName = "Sepatu", ProductPrice = 300000, Stock = 10 }
+        };
+
+            var querySyntaxProducts =
+                from product in products
+                orderby product.ProductName
+                select product.ProductName;
+
+            var methodSyntaxProducts =
+                products.Where(product => product.ProductPrice > 100000).Select(product => product.ProductName);
+
+            foreach (var querySyntaxProduct in querySyntaxProducts)
             {
-                Console.WriteLine("Ini Callback");
-            };
+                Console.WriteLine(querySyntaxProduct);
+            }
 
-            sayHello(callback);
+            foreach (var price in methodSyntaxProducts)
+            {
+                Console.WriteLine(price);
+            }
 
-            sayHello(()=> {
-                Console.WriteLine("Ini adalah Callback Anonymous Function");
-            });
+            //pagination menggunakan LINQ
+            var page = 2;
+            var size = 2;
+            var productsPage = products.Skip((page - 1) * size).Take(size);
 
+             Console.WriteLine($"Page {page}");
+            foreach (var product in productsPage)
+            {
+                Console.WriteLine(product);
+            }
         }
     }
 }
